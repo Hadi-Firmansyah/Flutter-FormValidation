9029ef8ddea1c79939452c5c6e22b828a2d9aca2
Hadi Firmansyah <hadifirmansyah.id@gmail.com>
2020-10-31 13:15:18 +0700

    First

---
+++ lib/src/mixins/validation.dart
@@ -0,0 +1,22 @@
+class Validation{
+  String validatePassword(String value){
+    if(value.length < 4){
+      return 'Password Minimal 6 Karakter';
+    }
+    return null;
+  }
+
+  String validateEmail(String value){
+    if(!value.contains('@')){
+      return 'Email tidak valid';
+    }
+    return null;
+  }
+
+  String validateName(String value){
+    if(value.isEmpty){
+      return 'Nama Lengkap Harus Di Isi';
+    }
+    return null;
+  }
+}
